7cac530c185764b7be7f1b033310f7e3e3f8a26a
Elmir <elmirbtj1@gmail.com>
2022-12-16 22:40:47 +0100

    final

--- interface_cleint.py
+++ interface_cleint.py
@@ -5,119 +5,42 @@ import sys
 import socket
 from threading import Thread
 from PyQt5.QtCore import QCoreApplication
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+from PyQt5 import QtCore
 
 
-        self.setGeometry(600, 300, 600, 500)
-        self.setWindowTitle("interface")
-        palette = QtGui.QPalette()
-        #icon = QtGui.QPixmap(r'.https://www.shutterstock.com/image-illustration/abstract-wave-technology-background-blue-260nw-2152448863.jpg')
-        #palette.setBrush(self.backgroundRole(), QtGui.QBrush(icon))
-        self.setPalette(palette)
 
-        self.setStyleSheet("""
-        QMainWindow{background:black}
-        """)
+
+class shell(QWidget):
+    def __init__(self,connection):
+        super(shell, self).__init__()
+        grid = QGridLayout()
+        self.setLayout(grid)
+        self.connnection =connection
 
         self.addUI()
-
-        self.client = socket.socket()
-        self.thread()
-        widget = QWidget()
-        self.setCentralWidget(widget)
-        self.connectionClosed = True
-        grid = QGridLayout()
-        widget.setLayout(grid)
-        self.__tabs = QTabWidget()
-        self.__tab1 = QWidget()
-        self.__tab2 = QWidget()
-        self.__tabs.addTab(self.__tab2, "Connection")
-        self.__tabs.addTab(self.__tab1, "Server_commande")
-
-        self.__tab1.layout = QGridLayout()
-
-        self.__tab1.layout.addWidget(self.text)
-        self.__tab1.setLayout(self.__tab1.layout)
-
-        self.__tab2.layout = QGridLayout()
-
-        self.__tab2.layout.addWidget(self.text)
-        self.__tab2.setLayout(self.__tab2.layout)
-
-        grid.addWidget(self.__tabs, 0, 0, 1, 2)
-        grid.addWidget(self.text, 3, 0)
-        self.__tab1.layout.addWidget(self.text, 2, 0)
-        self.__tab1.layout.addWidget(self.text2, 1, 0)
-        self.__tab1.layout.addWidget(self.button, 1, 1)
-
-
-        grid = QGridLayout()
-        widget.setLayout(grid)
-        self.__port = QLineEdit("5")
-        self.__ip = QLineEdit("localhost")
-
-
-        self.__port.setPlaceholderText("Port")
-        self.__ip.setPlaceholderText("IP")
-
-
-        self.__ok = QPushButton("Connection")
-
-        self.__tab2.layout.addWidget(self.__ip, 2, 0)
-        self.__tab2.layout.addWidget(self.__port, 1, 0)
-        self.__tab2.layout.addWidget(self.__ok, 1, 1)
-
-        self.__ok.clicked.connect(self.__lancement)
-
-        self.li = []
-
-        self.button.clicked.connect(self.send_msg)
         self.text2.returnPressed.connect(self.send_msg)
-        self.__threadecoute = Thread(target=self.send_msg())
+
+
+        self.connectionClosed =False
+
+        self.__threadecoute = Thread(target=self.recv_msg)
         self.__threadecoute.start()
 
-    def __lancement(self):
-        if len(self.__ip.text()) > 0 and self.__port.text().isdigit():
-            HOST = self.__ip.text()
-            PORT = int(self.__port.text())
-            try:
-                self.client = socket.socket()
-                self.client.connect((HOST, PORT))
-                self.connectionClosed = False
-                Thread(target=self.recv_msg).start()
-                self.__ip.setText("")
-                self.__port.setText("")
-            except:
-
-                print('ERREUR DE CONNECTION')
-        else:
-            print('ERREUR DE FORMULAIRE')
 
 
     def addUI(self):
 
-
         self.text = QTextEdit(self)
         self.text.verticalScrollBar().rangeChanged.connect(lambda: self.text.verticalScrollBar().setValue(self.text.verticalScrollBar().maximum()))
         self.text.setReadOnly(True)
-        self.text.setGeometry(10, 10, 359, 250)
+        self.text.setGeometry(10, 10, 570, 430)
         self.text.setStyleSheet('background-color:white;)')
 
         self.text2 = QLineEdit(self)
 
         self.text2.setPlaceholderText('Envoyer du contenu')
-        self.text2.setGeometry(10,260, 300, 30)
+        self.text2.setGeometry(10, 440, 570, 30)
         self.text2.setStyleSheet('background-color:white;)}')
-
-
-        self.button = QPushButton('envoyer', self)
-        self.button.setFont(QFont('', 8, QFont.Bold))
-        self.button.setGeometry(310, 260, 60, 30)
-
-
 
     def send_msg(self):
         if len(self.text2.text()) > 0:
@@ -127,30 +50,135 @@ class MainWindow(QMainWindow):
                 try:
                     print(msg)
                     if msg != "":
-                        self.client.send(msg.encode())
+                        self.connnection.send(msg.encode())
                 except:
                     self.text.append('Impossible de communiquer avec le server! \n')
             else:
                 self.text.append('Déconnecté. \n')
             self.text2.clear()
 
-
     def recv_msg(self):
         while not self.connectionClosed:
             try:
-                data = self.client.recv(1024).decode()
+                data = self.connnection.recv(1024).decode()
                 self.text.append(data + '\n')
                 if data == 'DISCONNECT':
-                    self.client.close()
-                    self.connectionClosed = True
+
+                    self.close()
             except:
                 pass
 
 
+    def close(self):
+        self.connectionClosed=True
+        self.connnection.close()
+        self.deleteLater()
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(600, 300, 600, 500)
+        self.setWindowTitle("interface")
+        palette = QtGui.QPalette()
+        self.setPalette(palette)
+
+        self.setStyleSheet("""
+        QMainWindow{background:white}
+        """)
+
+        widget = QWidget()
+        self.setCentralWidget(widget)
+        grid = QGridLayout()
+        widget.setLayout(grid)
+
+        self.__tabs = QTabWidget()
+        self.__tab1 = QWidget()
+        self.__tabs.addTab(self.__tab1,"Connection")
+        self.create_table()
+        self.__tabs.addTab(self.table,"CSV")
+        grid.setContentsMargins(0,0,0,0)
+        grid.setSpacing(0)
+
+        self.__tab1.layout = QGridLayout()
+        self.__tab1.setLayout(self.__tab1.layout)
+        grid.addWidget(self.__tabs, 0, 0, 0, 0)
+
+        grid = QGridLayout()
+        widget.setLayout(grid)
+        self.__port = QLineEdit("5")
+        self.__ip = QLineEdit("localhost")
+
+        self.__port.setPlaceholderText("Port")
+        self.__ip.setPlaceholderText("IP")
+        self.__ok = QPushButton("Connection")
+
+        self.__tab1.layout.addWidget(self.__ip, 1, 0)
+        self.__tab1.layout.addWidget(self.__port, 1, 1)
+        self.__tab1.layout.addWidget(self.__ok, 1, 2)
+        self.__ok.clicked.connect(self.__start)
+        self.connections = []
+
+
+    def create_table(self):
+        self.table = QTableWidget()
+        self.table.setRowCount(0)
+        self.table.setColumnCount(1)
+
+        try:
+            with open('test.csv','r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    line =line.replace('\n', '')
+                    if len(line.split(',')) == 2 and line.split(',')[1].isdigit() and len(line.split(',')[0]) > 0:
+                        HOST = line.split(',')[0]
+                        PORT = line.split(',')[1]
+
+                        row = self.table.rowCount()
+                        self.table.setRowCount(row + 1)
+                        self.table.setItem(row, 0, QTableWidgetItem(HOST + ':' + PORT))
+        except:
+            with open('test.csv','w') as file :
+                file.write('')
+
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.verticalHeader().setVisible(False)
+        self.table.horizontalHeader().setVisible(False)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
+        self.table.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table.resizeRowsToContents()
+
+
+    def __start(self):
+        if len(self.__ip.text()) > 0 and self.__port.text().isdigit():
+            HOST = self.__ip.text()
+            PORT = int(self.__port.text())
+            try:
+                client = socket.socket()
+                client.connect((HOST,PORT))
+                tab = shell(connection=client)
+                self.__tabs.addTab(tab, f'{HOST}:{PORT}')
+                self.connections.append(tab)
+
+            except:
+
+                print('ERREUR DE CONNECTION')
+        else:
+            print('ERREUR DE FORMULAIRE')
+
+
+
+
+
 
     def closeEvent(self, QCloseEvent):
-        self.connectionClosed = True
-        self.client.close()
+        for i in self.connections :
+            try :
+                i.close()
+            except:
+                pass
         QCloseEvent.accept()
         QCoreApplication.exit(0)
 
--- servprof.py
+++ servprof.py
@@ -2,9 +2,7 @@ import socket
 import sys
 import os , subprocess ,psutil
 
-m_kill = "kill"
-m_disconnect = "disconnect"
-m_reset = "reset"
+
 
 host = "localhost"
 port = int(input("Mettez votre port :"))
@@ -12,11 +10,8 @@ port = int(input("Mettez votre port :"))
 message_client = ""
 
 def help():
-    help =["ip" , "hostname","ram","cpu","os"]
-    re =""
-    for element in help:
-       re = element
-    return re
+
+    return ("""COMMANDE: --ip-- -- hostname -- -- ram-- -- os -- -- cpu -- -- kill -- -- reset -- -- disconnect \n- ping - PYHTON -VERSION - DOS: - linux:- PowerShell: """)
 
 def ip():
     name = socket.gethostname()
@@ -33,113 +28,136 @@ def ram():
     ram1 = cmd[0] / 1000000000
     ram2 = cmd[1] / 1000000000
     ram3 = cmd[3] / 1000000000
-    return f"Memoire Total :{ram1}\n " \
-           f"Memoire utilisée :{ram2} \n" \
+    return f"Memoire Total :{ram1}\n"\
+           f"Memoire utilisée :{ram2}\n"\
            f"Memoire libre :{ram3}"
 
+def DOS(cmd):
 
-def stockage():
-    stockage = psutil.disk_usage("/")
-    stockage1 = stockage[0] / 1000000000
-    stockage2 = stockage[1] / 1000000000
-    stockage3 = stockage[2] / 1000000000
-    return f"Stockage total {stockage1}\n  , {stockage2}\n , {stockage3}"
+    try:
+        if sys.platform == "win32":
+            re = cmd.split(':')[1]
+            res = subprocess.check_output(re, shell=True).decode("cp850")
+            return res
+        else:
+            return "DOS n'est pas correcte essayer linux ou PowerShell"
+    except:
+        return 'Error command'
+
+def linux(cmd):
+    try:
+        if sys.platform == "linux" or sys.platform == "linux2":
+            x = cmd.split(":", 1)[1]
+            res= subprocess.getoutput(x)
+            return res
+        else:
+            return "Linux n'est pas correcte essayer DOS ou PowerShell"
+
+    except:
+        return 'Error command'
+
+def powershell(cmd):
+    try:
+        if sys.platform == "win32":
+            shell = cmd.split(":", 1)[1]
+            res = subprocess.getoutput('PowerShell -command "' + shell + '"')
+            return res
+        else:
+            return 'PowerShell commands are not recognized try Linux'
+    except:
+        return 'Error command'
 
 
 
 def OS():
-    cmd=sys.platform
-    if cmd == "win32":
-        #print("voici l'OS de votre pc")
+    res = sys.platform
+    if res == 'win32':
+        res = 'La machine est un Windows'
+    elif res == 'linux' or res == 'linux2':
+        res = 'La machine est un Linux'
+    elif res == 'darwin':
+        res = 'La machine est un MAC OS '
+    return res
 
-        return cmd
 
 def cpu ():
-    cpu = psutil.cpu_count()
-    #cmd = str(subprocess.check_output("wmic cpu get caption, deviceid, name, numberofcores, maxclockspeed, status", shell=True))
-    return cpu
+    res = psutil.cpu_percent()
+    return f'Capaciter du CPU: {res} %'
+
 
 def execute(cmd):
     if cmd == 'cpu':
         res = cpu()
-
         print(f'voici le cpu de la machine: {res}')
 
     elif cmd == 'os':
         res = OS()
-
         print(f"L OS est un {res}")
 
     elif cmd == 'ram':
         res = ram()
-
         print(f"ram {res}")
-
-
-    elif cmd == 'stockage':
-        res = stockage()
-
-        print(f"{res}")
 
     elif cmd == 'ip':
         res = ip()
-
         print(f"{res}")
 
     elif cmd == 'help':
         res = help()
-
         print(f"{res}")
 
     elif cmd == 'hostname':
         res = nom()
-
         print(f"{res}")
 
     elif cmd[0:4] == "DOS:":
-        re = cmd.split(':')[1]
-        res = subprocess.check_output(re,shell =True).decode("cp850")
-
+        res = DOS(cmd)
         return f"{res}"
 
+    elif cmd[0:6].lower() == 'linux:':
+        res = linux(cmd)
+        return f"{res}"
+
+    elif cmd[0:11].lower() == 'powershell:':
+        res = powershell(cmd)
+        return f"{res}"
     elif cmd[0:4] == "ping":
         re = cmd.split(' ')[1]
         res = subprocess.getoutput(cmd)
-
         return f"{res}"
-
-
-
     else :
         res = "Unknown Command"
-        return str(res)
+    return str(res)
 
 
 
 
 def serveur():
     message_client = ""
-    while message_client != "kill":
+    while message_client.lower() != "kill":
 
         message_client = ""
-        while message_client != "kill":
+        while message_client.lower() != "kill":
             server_socket = socket.socket()
             server_socket.bind((host, port))
             server_socket.listen(2)
             print(f"Socket ouverte sur {host} - {port}")
 
             message_client = ""
-            while message_client != "kill" and message_client != "reset":
-                print('FD> En attente du client')
+            while message_client.lower() != "kill" and message_client.lower() != "reset":
+                print('> En attente du client')
                 conn, address_client = server_socket.accept()
                 print(f'Client connecté {address_client}')
+                try:
+                    message_client = ""
+                    while message_client.lower() != "kill" and message_client.lower() != "reset" and message_client.lower() != "disconnect":
 
-                message_client = ""
-                while message_client.lower() != m_kill and message_client.lower() != m_reset and message_client.lower() != m_disconnect:
-                    message_client = conn.recv(1024).decode()
-                    print(f"Message reçu {message_client}")
-                    execution = execute(message_client)
-                    conn.send(execution.encode())
+                        message_client = conn.recv(1024).decode()
+                        print(f"Message reçu {message_client}")
+                        execution = execute(message_client)
+                        conn.send(execution.encode())
+                except:
+                    pass
                 try:
                     conn.send('DISCONNECT'.encode())
                 except:
